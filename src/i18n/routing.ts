5262a168ebe5c021e8ef2091e66648bf8c7a4a0d
Mohammad Ali <ma.jamei@raymon.info>
2025-03-03 17:08:29 +0330

    i18n added to project.

---
+++ src/i18n/routing.ts
@@ -0,0 +1,7 @@
+import {defineRouting} from 'next-intl/routing';
+import {createNavigation} from 'next-intl/navigation';
+ 
+export const routing = defineRouting({
+  locales: ['fa', 'en'],
+  defaultLocale: 'fa'
+});
